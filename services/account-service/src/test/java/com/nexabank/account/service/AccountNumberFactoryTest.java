7299e0dfbe5cc1f991320dc34f953c9253b603e6
acjoyner <anthony.c.joyner@gmail.com>
2026-04-17 19:03:18 -0400

    Fix AccountNumberFactoryTest to use Mockito for AccountRepository dependency

--- services/account-service/src/test/java/com/nexabank/account/service/AccountNumberFactoryTest.java
+++ services/account-service/src/test/java/com/nexabank/account/service/AccountNumberFactoryTest.java
@@ -1,23 +1,36 @@
 package com.nexabank.account.service;
 
 import com.nexabank.account.model.AccountType;
+import com.nexabank.account.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AccountNumberFactoryTest {
+
+    @Mock
+    private AccountRepository accountRepository;
 
     private AccountNumberFactory factory;
 
+    private long count = 0;
+
     @BeforeEach
     void setUp() {
-        factory = new AccountNumberFactory();
+        when(accountRepository.count()).thenAnswer(inv -> count++);
+        factory = new AccountNumberFactory(accountRepository);
     }
 
     @Test
